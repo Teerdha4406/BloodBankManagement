9ee52948114fd2781ed3c5dd23fcbbdd7b357d81
Teerdha Ganesh <168392047+Teerdha4406@users.noreply.github.com>
2024-11-07 22:14:52 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,39 +5,23 @@ import Home from "./Component/home";
 import Doner from "./Component/Doner";
 import DonerList from "./Component/DonerList";
 import Footer from "./Component/Footer";
-import LoginComponent from "./Component/loginComponent";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
-import SignUpComponent from "./Component/signUPComponent";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [logedInUser, setLogedInUser] = useState(false);
-
   return (
     <Router>
       <div className="App">
-        {!logedInUser ? (
-          <Routes>
-            <Route
-              path="/login"
-              element={<LoginComponent setLogedInUser={setLogedInUser} />}
-            />
-            <Route path="/signup" element={<SignUpComponent />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-        ) : (
-          <>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/About" element={<About />} />
-              <Route path="/Doner" element={<Doner />} />
-              <Route path="/DonerList" element={<DonerList />} />
-              <Route path="*" element={<Home />} /> {/* Fallback route */}
-            </Routes>
-            <Footer />
-          </>
-        )}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Doner" element={<Doner />} />
+          <Route path="/DonerList" element={<DonerList />} />
+          {/* If you want to include login and signup as part of the app navigation */}
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/signup" element={<SignUpComponent />} />
+        </Routes>
+        <Footer />
       </div>
     </Router>
   );
