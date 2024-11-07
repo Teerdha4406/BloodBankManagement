80f9eb1cf4ef1d6240be5e76ef3120d64c727bdb
Teerdha Ganesh <168392047+Teerdha4406@users.noreply.github.com>
2024-11-07 22:09:40 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Doner from "./Component/Doner";
 import DonerList from "./Component/DonerList";
 import Footer from "./Component/Footer";
 import LoginComponent from "./Component/loginComponent";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import SignUpComponent from "./Component/signUPComponent";
 
@@ -15,26 +15,30 @@ function App() {
 
   return (
     <Router>
-      {!logedInUser ? (
-        <Routes>
-          <Route
-            path="/login"
-            element={<LoginComponent setLogedInUser={setLogedInUser} />}
-          />
-          <Route path="/signup" element={<SignUpComponent />} />
-        </Routes>
-      ) : (
-        <>
-          <Navbar />
+      <div className="App">
+        {!logedInUser ? (
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Doner" element={<Doner />} />
-            <Route path="/DonerList" element={<DonerList />} />
+            <Route
+              path="/login"
+              element={<LoginComponent setLogedInUser={setLogedInUser} />}
+            />
+            <Route path="/signup" element={<SignUpComponent />} />
+            <Route path="*" element={<Navigate to="/login" />} />
           </Routes>
-          <Footer />
-        </>
-      )}
+        ) : (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Doner" element={<Doner />} />
+              <Route path="/DonerList" element={<DonerList />} />
+              <Route path="*" element={<Home />} /> {/* Fallback route */}
+            </Routes>
+            <Footer />
+          </>
+        )}
+      </div>
     </Router>
   );
 }
