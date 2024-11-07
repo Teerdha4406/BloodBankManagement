536cb0236b348dcc8898681f1b18198a780c4c33
Teerdha Ganesh <168392047+Teerdha4406@users.noreply.github.com>
2024-11-07 22:04:09 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,40 +9,33 @@ import LoginComponent from "./Component/loginComponent";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import SignUpComponent from "./Component/signUPComponent";
+
 function App() {
   const [logedInUser, setLogedInUser] = useState(false);
-  const handleloggedIn = () => {
-    // eslint-disable-next-line no-restricted-globals
-    history.pushState("", "", "/login");
-    return (
-      <Routes>
-        <Route
-          path="/login"
-          element={<LoginComponent setLogedInUser={setLogedInUser} />}
-        />
-        <Route path="/signup" element={<SignUpComponent />} />
-      </Routes>
-    );
-  };
+
   return (
-    <>
-      <Router>
-        {!logedInUser ? (
-          handleloggedIn()
-        ) : (
-          <>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/About" element={<About />} />
-              <Route path="/Doner" element={<Doner />} />
-              <Route path="/DonerList" element={<DonerList />} />
-            </Routes>
-            <Footer />
-          </>
-        )}
-      </Router>
-    </>
+    <Router>
+      {!logedInUser ? (
+        <Routes>
+          <Route
+            path="/login"
+            element={<LoginComponent setLogedInUser={setLogedInUser} />}
+          />
+          <Route path="/signup" element={<SignUpComponent />} />
+        </Routes>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Doner" element={<Doner />} />
+            <Route path="/DonerList" element={<DonerList />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
+    </Router>
   );
 }
 
